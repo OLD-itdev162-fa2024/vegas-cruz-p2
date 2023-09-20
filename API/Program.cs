d19dac707bc625b5ef160423093c3684be31c115
vegascruz <cruzv@students.westerntc.edu>
2023-09-19 20:17:29 -0500

    Halfway through the activity

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
-
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
