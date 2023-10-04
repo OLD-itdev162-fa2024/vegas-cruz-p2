f631913ff768546024c5746e480cb0dc3c415852
vegascruz <cruzv@students.westerntc.edu>
2023-10-03 20:45:03 -0500

    Created the post controller

---
+++ Persistence/Seed.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public interface Seed
+    {
+        
+        public static void SeedData(DataContext context){
+            if(!context.Posts.Any())
+            {
+                var Posts = new List<Post>
+                {
+                    new Post {
+                        Title = "First post",
+                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        Date = DateTime.Now.AddDays(-10)
+                    },
+                    new Post {
+                        Title = "Second post",
+                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        Date = DateTime.Now.AddDays(-7)
+                    },
+                    new Post {
+                        Title = "Third post",
+                        Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        Date = DateTime.Now.AddDays(-4)
+                    }
+                };
+
+                context.Posts.AddRange(Posts);
+                context.SaveChanges();
+            }
+        }
+    }
+}
